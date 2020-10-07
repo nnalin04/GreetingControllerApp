fc9daa5c73916f8e943e50e972451799e874ff5c
nnalin04 <nnalin04@gmail.com>
2020-10-08 00:21:06 +0530

    Refactored code for method to handle both the user and the greeting DTO

--- src/main/java/com/bridgelabz/greetingcontroller/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingcontroller/service/GreetingService.java
@@ -1,38 +1,51 @@
 package com.bridgelabz.greetingcontroller.service;
 
+import com.bridgelabz.greetingcontroller.dto.GreetingDTO;
 import com.bridgelabz.greetingcontroller.dto.UserDTO;
+import com.bridgelabz.greetingcontroller.exception.GreetingMessageException;
 import com.bridgelabz.greetingcontroller.modle.GreetingMessage;
 import com.bridgelabz.greetingcontroller.repository.IGreetingRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class GreetingService implements IGreetingService{
 
     @Autowired
     IGreetingRepository greetingRepository;
 
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Override
     public GreetingMessage createMessage(UserDTO userDTO) {
         GreetingMessage greetingMessage = new GreetingMessage();
-        greetingMessage.setGreetingMessage(getMessage(userDTO));
-        GreetingMessage message = greetingRepository.save(greetingMessage);
-        return message;
+        greetingMessage.setGreetingMessage(getMessage(userDTO.getMessage(),
+                userDTO.getFirstName(), userDTO.getLastName()));
+        return greetingRepository.save(greetingMessage);
     }
 
-    public String getMessage(UserDTO userDTO) {
-        if (userDTO.getFirstName() != null && userDTO.getLastName() != null)
-           return "Hello "+ userDTO.getFirstName() + " "+ userDTO.getLastName();
-        if (userDTO.getFirstName() != null && userDTO.getLastName() == null)
-            return "Hello "+ userDTO.getFirstName() ;
-        if (userDTO.getFirstName() == null && userDTO.getLastName() != null)
-            return "Hello "+ userDTO.getLastName();
+    public String getMessage(String message, String firstName, String lastName) {
+        if (message != null)
+            return message;
+        if (firstName != null && lastName != null)
+           return "Hello "+ firstName + " "+ lastName;
+        if (firstName != null)
+            return "Hello "+ firstName ;
+        if (lastName != null)
+            return "Hello "+ lastName;
         return "Hello World";
     }
 
-    public GreetingMessage findById(Long id) {
-        return greetingRepository.findById(id).get();
+    @Override
+    public GreetingMessage findMessageById(Long id) {
+        return greetingRepository.findById(id).orElseThrow(
+                () -> new GreetingMessageException("Record not found"));
     }
 
     @Override
@@ -41,9 +54,18 @@ public class GreetingService implements IGreetingService{
     }
 
     @Override
-    public GreetingMessage updateMessage(Long id, UserDTO userDTO) {
-        GreetingMessage greetingMessage = findById(id);
-        greetingMessage.setGreetingMessage(getMessage(userDTO));
+    public GreetingMessage updateMessage(GreetingDTO greetingDTO) {
+        findMessageById(greetingDTO.getGreetingId());
+        GreetingMessage greetingMessage = new GreetingMessage();
+        greetingMessage.setGreetingId(greetingDTO.getGreetingId());
+        greetingMessage.setGreetingMessage( getMessage( greetingDTO.getMessage(),
+                greetingDTO.getFirstName(), greetingDTO.getLastName()));
         return greetingRepository.save(greetingMessage);
+    }
+
+    @Override
+    public String deleteMessage(Long id) {
+        greetingRepository.delete(findMessageById(id));
+        return "Successfully deleted";
     }
 }
