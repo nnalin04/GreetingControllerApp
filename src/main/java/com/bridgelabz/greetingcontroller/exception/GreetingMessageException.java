4522e17b3d4ea38332b94a6b046ab4a94c4f7eea
nnalin04 <nnalin04@gmail.com>
2020-10-08 00:14:57 +0530

    Added code to handle exception while doing get Request

---
+++ src/main/java/com/bridgelabz/greetingcontroller/exception/GreetingMessageException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.greetingcontroller.exception;
+
+public class GreetingMessageException extends RuntimeException {
+    public GreetingMessageException(String message) {
+        super(message);
+    }
+}
