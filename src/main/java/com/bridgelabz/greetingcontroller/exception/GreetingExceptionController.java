c598c25bca2eb426201a84089f1e868865f318e9
nnalin04 <nnalin04@gmail.com>
2020-10-08 00:50:01 +0530

    Added code to handle the greeting controller exception

---
+++ src/main/java/com/bridgelabz/greetingcontroller/exception/GreetingExceptionController.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.greetingcontroller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GreetingExceptionController extends RuntimeException{
+    @ExceptionHandler(value = GreetingMessageException.class)
+    public ResponseEntity<Object> exception(GreetingMessageException e) {
+        return new ResponseEntity<>("Record not found", HttpStatus.NOT_FOUND);
+    }
+}
